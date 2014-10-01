32a755d52e36aad87060f7d15bb774f25aa3b695
Jeff Martin <jeffam71@gmail.com>
2014-09-30 21:59:11 -0700

    Updated IP_Loader.spin and IP host software to transmit packets and acknowledgements in the form beginning with PacketID then TransID.

--- Main.pas
+++ Main.pas
@@ -563,28 +563,28 @@ const
        SendDebugMessage('+' + GetTickDiff(STime, Ticks).ToString + ' Transmitting packet ' + PacketID.ToString, True);
 
        Rnd := Random($FFFFFFFF);                                                                 {  Generate random Transmission ID}
-       Move(Rnd, TxBuf[0], 4);                                                                   {  Store Random Transmission ID}
+       Move(Rnd, TxBuf[4], 4);                                                                   {  Store Random Transmission ID}
 
        if not XBee.SendUDP(TxBuf, True, False) then
          raise EHardDownload.Create('Error: Can not transmit packet!');
 
        SendDebugMessage('+' + GetTickDiff(STime, Ticks).ToString + ' - Waiting for packet acknowledgement', True);
 
-       repeat                                                                                    {  Wait for positive/negative acknowledgement of this}
-         Acknowledged := not IgnoreResponse and XBee.ReceiveUDP(RxBuf, DynamicSerTimeout);       {  specific transmission, or timeout.  This loop throws}
-       until not Acknowledged or ((Length(RxBuf) = 8) and (Long(@RxBuf[4]) = Long(@TxBuf[0])));  {  out acknowledgements to previous transmissions received now.}
+       repeat                                                                                    {  Wait for positive/negative acknowledgement of this specific}
+         Acknowledged := not IgnoreResponse and XBee.ReceiveUDP(RxBuf, DynamicSerTimeout);       {  transmission, or timeout if none.  This loop throws out}
+       until not Acknowledged or ((Length(RxBuf) = 8) and (Long(@RxBuf[4]) = Long(@TxBuf[4])));  {  acknowledgements to previous transmissions, received late.}
 
-       Acknowledged := Acknowledged and (Long(@RxBuf[0]) <> Long(@TxBuf[4]));
+       Acknowledged := Acknowledged and (Long(@RxBuf[0]) <> Long(@TxBuf[0]));                    {  Amend Acknowledged flag with response's ACK/NAK status}
 
-       dec(Retry);                                                                               {  Loop and retransmit until timely non-retransmit acknowledgement received, or retry count exhausted}
+       dec(Retry);
 
      {Repeat - (Re)Transmit packet...}
-     until IgnoreResponse or Acknowledged or (Retry = 0);
+     until IgnoreResponse or Acknowledged or (Retry = 0);                                        {Loop and retransmit until proper acknowledgement received, or retry count exhausted}
 
      if not (IgnoreResponse or Acknowledged) then
        raise EHardDownload.Create('Error: connection lost!');                                    {No acknowledgement received? Error}
 
-     Result := IfThen(not IgnoreResponse, Long(@RxBuf[0]), 0);
+     Result := IfThen(not IgnoreResponse, Long(@RxBuf[0]), 0);                                   {Return acknowledgement's requested Packet ID value}
    end;
 
    {----------------}
@@ -714,7 +714,7 @@ begin
         repeat {Transmit target application packets}                                             {Transmit application image}
           TxBuffLength := 2 + Min((XBee.MaxDataSize div 4)-2, FBinSize - i);                     {  Determine packet length (in longs); header + packet limit or remaining data length}
           SetLength(TxBuf, TxBuffLength*4);                                                      {  Set buffer length (Packet Length) (in longs)}
-          Move(PacketID, TxBuf[4], 4);                                                           {  Store Packet ID (skipping over Transmission ID field)}
+          Move(PacketID, TxBuf[0], 4);                                                           {  Store Packet ID (skip over Transmission ID field; "TransmitPacket" will fill that)}
           Move(FBinImage[i*4], TxBuf[8], (TxBuffLength-2)*4);                                    {  Store section of data}
           UpdateProgress(0, 'Sending packet: ' + (TotalPackets-PacketID+1).ToString + ' of ' + TotalPackets.ToString);
           if TransmitPacket <> PacketID-1 then                                                   {  Transmit packet (retransmit as necessary)}
@@ -1079,29 +1079,29 @@ const
   it assists with the remainder of the download (at a faster speed and with more relaxed interstitial timing conducive of Internet Protocol delivery.
   This memory image isn't used as-is; before download, it is first adjusted to contain special values assigned by this host (communication timing and
   synchronization values) and then is translated into an optimized Propeller Download Stream understandable by the Propeller ROM-based boot loader.}
-  RawLoaderImage : array[0..363] of byte = ($00,$B4,$C4,$04,$6F,$48,$10,$00,$6C,$01,$74,$01,$64,$01,$78,$01,
-                                            $5C,$01,$02,$00,$54,$01,$00,$00,$4C,$E8,$BF,$A0,$4C,$EC,$BF,$A0,
-                                            $4D,$62,$BF,$A1,$01,$62,$FF,$28,$F1,$63,$BF,$80,$A0,$60,$CF,$A0,
-                                            $4D,$62,$BF,$F8,$F2,$97,$3C,$61,$05,$60,$FF,$E4,$52,$22,$FC,$54,
-                                            $02,$66,$FF,$A0,$4D,$62,$BF,$A0,$F1,$63,$BF,$80,$04,$68,$FF,$A0,
-                                            $08,$6A,$FF,$A0,$4D,$62,$BF,$F8,$4C,$E8,$BF,$64,$01,$A4,$FC,$21,
-                                            $4D,$62,$BF,$F8,$4C,$E8,$BF,$70,$11,$6A,$FF,$E4,$4D,$62,$BF,$F8,
-                                            $4C,$E8,$BF,$68,$0E,$68,$FF,$E4,$47,$22,$BC,$80,$0D,$66,$FF,$E4,
-                                            $4E,$9A,$BC,$A0,$50,$42,$FC,$50,$53,$5E,$FF,$A0,$AF,$5D,$BC,$54,
-                                            $AF,$5F,$BC,$54,$AF,$61,$BC,$54,$04,$68,$FF,$A0,$50,$60,$BF,$A0,
-                                            $4F,$62,$BF,$A1,$00,$64,$FF,$A0,$80,$64,$FF,$72,$F2,$97,$3C,$61,
-                                            $24,$60,$FB,$E4,$35,$00,$78,$5C,$F1,$63,$BF,$80,$4D,$62,$BF,$F8,
-                                            $F2,$97,$3C,$61,$00,$65,$FF,$70,$01,$64,$FF,$29,$29,$00,$4C,$5C,
-                                            $FF,$A6,$FC,$64,$B2,$A7,$BC,$68,$08,$A6,$FC,$20,$51,$42,$FC,$50,
-                                            $21,$68,$FF,$E4,$01,$5E,$FF,$80,$1D,$00,$7C,$5C,$21,$60,$BF,$A0,
-                                            $FF,$61,$FF,$60,$50,$60,$7F,$86,$00,$8C,$68,$0C,$52,$A8,$3C,$C2,
-                                            $09,$00,$54,$5C,$01,$A4,$FC,$C1,$55,$00,$70,$5C,$55,$5E,$FF,$84,
-                                            $44,$AA,$3C,$08,$04,$88,$FC,$80,$47,$7C,$BC,$80,$3E,$5E,$FF,$E4,
-                                            $55,$7C,$FC,$54,$09,$00,$7C,$5C,$00,$00,$00,$00,$00,$00,$00,$00,
-                                            $80,$00,$00,$00,$00,$02,$00,$00,$00,$80,$00,$00,$FF,$FF,$F9,$FF,
-                                            $10,$C0,$07,$00,$00,$00,$00,$80,$00,$00,$00,$40,$B6,$02,$00,$00,
-                                            $5B,$01,$00,$00,$08,$02,$00,$00,$55,$73,$CB,$00,$50,$45,$01,$00,
-                                            $00,$00,$00,$00,$35,$C7,$08,$35,$2C,$32,$00,$00);
+  RawLoaderImage : array[0..367] of byte = ($00,$B4,$C4,$04,$6F,$DA,$10,$00,$70,$01,$78,$01,$68,$01,$7C,$01,
+                                            $60,$01,$02,$00,$58,$01,$00,$00,$4D,$E8,$BF,$A0,$4D,$EC,$BF,$A0,
+                                            $4E,$AC,$BC,$A1,$01,$AC,$FC,$28,$F1,$AD,$BC,$80,$A0,$AA,$CC,$A0,
+                                            $4E,$AC,$BC,$F8,$F2,$99,$3C,$61,$05,$AA,$FC,$E4,$53,$24,$FC,$54,
+                                            $5C,$A8,$BC,$A0,$02,$B0,$FC,$A0,$4E,$AC,$BC,$A0,$F1,$AD,$BC,$80,
+                                            $04,$B2,$FC,$A0,$08,$B4,$FC,$A0,$4E,$AC,$BC,$F8,$4D,$E8,$BF,$64,
+                                            $01,$A6,$FC,$21,$4E,$AC,$BC,$F8,$4D,$E8,$BF,$70,$12,$B4,$FC,$E4,
+                                            $4E,$AC,$BC,$F8,$4D,$E8,$BF,$68,$0F,$B2,$FC,$E4,$48,$24,$BC,$80,
+                                            $0E,$B0,$FC,$E4,$4F,$9C,$BC,$A0,$51,$44,$FC,$50,$5B,$A8,$FC,$A0,
+                                            $54,$5E,$BC,$54,$54,$60,$BC,$54,$54,$62,$BC,$54,$04,$B2,$FC,$A0,
+                                            $51,$AA,$BC,$A0,$50,$AC,$BC,$A1,$00,$AE,$FC,$A0,$80,$AE,$FC,$72,
+                                            $F2,$99,$3C,$61,$25,$AA,$F8,$E4,$36,$00,$78,$5C,$F1,$AD,$BC,$80,
+                                            $4E,$AC,$BC,$F8,$F2,$99,$3C,$61,$00,$AF,$FC,$70,$01,$AE,$FC,$29,
+                                            $2A,$00,$4C,$5C,$FF,$B6,$FC,$64,$57,$B6,$BC,$68,$08,$B6,$FC,$20,
+                                            $52,$44,$FC,$50,$22,$B2,$FC,$E4,$01,$A8,$FC,$80,$1E,$00,$7C,$5C,
+                                            $22,$AA,$BC,$A0,$FF,$AB,$FC,$60,$51,$AA,$7C,$86,$00,$8E,$68,$0C,
+                                            $53,$B6,$3C,$C2,$09,$00,$54,$5C,$01,$A6,$FC,$C1,$5D,$00,$70,$5C,
+                                            $5D,$A8,$FC,$84,$45,$BA,$3C,$08,$04,$8A,$FC,$80,$48,$7E,$BC,$80,
+                                            $3F,$A8,$FC,$E4,$5D,$7E,$FC,$54,$09,$00,$7C,$5C,$00,$00,$00,$00,
+                                            $00,$00,$00,$00,$80,$00,$00,$00,$00,$02,$00,$00,$00,$80,$00,$00,
+                                            $FF,$FF,$F9,$FF,$10,$C0,$07,$00,$00,$00,$00,$80,$00,$00,$00,$40,
+                                            $B6,$02,$00,$00,$5B,$01,$00,$00,$08,$02,$00,$00,$55,$73,$CB,$00,
+                                            $50,$45,$01,$00,$00,$00,$00,$00,$35,$C7,$08,$35,$2C,$32,$00,$00);
 
   RawLoaderInitOffset = -8*4;       {Offset (in bytes) from end of Loader Image (above) to where the host-initialized values exist within it.
                                     Host-Initialized values are constants in the Propeller Assembly code that are intended to be replaced
@@ -1112,18 +1112,18 @@ const
   MaxRxSenseError = 23;             {Maximum number of cycles by which the detection of a start bit could be off (as affected by the Loader code)}
 
   {Loader VerifyRAM snippet}
-  VerifyRAM : array[0..67] of byte = ($48,$66,$BF,$A0,$44,$66,$BF,$84,$02,$66,$FF,$2A,$44,$8A,$14,$08,
-                                     $04,$88,$D4,$80,$58,$66,$D7,$E4,$0A,$66,$FF,$04,$04,$66,$FF,$84,
-                                     $B3,$93,$3C,$08,$04,$66,$FF,$84,$B3,$93,$3C,$08,$01,$88,$FC,$84,
-                                     $44,$68,$BF,$00,$B4,$8B,$BC,$80,$60,$88,$7C,$E8,$45,$A4,$BC,$A4,
-                                     $09,$00,$7C,$5C);
+  VerifyRAM : array[0..67] of byte = ($49,$B0,$BC,$A0,$45,$B0,$BC,$84,$02,$B0,$FC,$2A,$45,$8C,$14,$08,
+                                      $04,$8A,$D4,$80,$60,$B0,$D4,$E4,$0A,$B0,$FC,$04,$04,$B0,$FC,$84,
+                                      $58,$94,$3C,$08,$04,$B0,$FC,$84,$58,$94,$3C,$08,$01,$8A,$FC,$84,
+                                      $45,$B2,$BC,$00,$59,$8C,$BC,$80,$68,$8A,$7C,$E8,$46,$A6,$BC,$A4,
+                                      $09,$00,$7C,$5C);
 
   {Loader LaunchStart snippet}
-  LaunchStart : array[0..27] of byte = ($B8,$70,$FC,$58,$58,$70,$FC,$50,$09,$00,$7C,$5C,$06,$88,$FC,$04,
-                                        $10,$88,$7C,$86,$00,$8C,$54,$0C,$02,$94,$7C,$0C);
+  LaunchStart : array[0..27] of byte = ($B8,$72,$FC,$58,$60,$72,$FC,$50,$09,$00,$7C,$5C,$06,$8A,$FC,$04,
+                                        $10,$8A,$7C,$86,$00,$8E,$54,$0C,$02,$96,$7C,$0C);
 
   {Loader LaunchFinal snippet}
-  LaunchFinal : array[0..15] of byte = ($06,$88,$FC,$04,$10,$88,$7C,$86,$00,$8C,$54,$0C,$02,$94,$7C,$0C);
+  LaunchFinal : array[0..15] of byte = ($06,$8A,$FC,$04,$10,$8A,$7C,$86,$00,$8E,$54,$0C,$02,$96,$7C,$0C);
 
   {Loader executable snippets}
   ExeSnippet : array[ltVerifyRAM..ltLaunchFinal] of PByteArray =  (@VerifyRAM, @VerifyRAM, @LaunchStart, @LaunchFinal);
@@ -1223,7 +1223,7 @@ begin
     begin
     {Prepare loader's executable packet}
     SetLength(TxBuf, 2*4+ExeSnippetSize[LoaderType]);                                                               {Set packet size for executable packet}
-    Move(PacketID, TxBuf[4], 4);                                                                                    {Store Packet ID; skip over Transmission ID}
+    Move(PacketID, TxBuf[0], 4);                                                                                    {Store Packet ID (skip over Transmission ID field; "TransmitPacket" will fill that)}
     Move(ExeSnippet[LoaderType][0], TxBuf[8], ExeSnippetSize[LoaderType]);                                          {and copy the packet code to it}
     end;
 end;
